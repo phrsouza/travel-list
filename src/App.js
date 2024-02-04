c20dc3c8cf33543736a3bbecf7b04bfb4ce744e3
Fauno <pedrohenriqueramossouza@gmail.com>
2024-02-04 11:09:09 -0300

    add list and form structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -14,15 +19,47 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div className="add-form">
-      <h3>what do you need for you</h3>
-    </div>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>what do you need for your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
