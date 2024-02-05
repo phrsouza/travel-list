1f80ba2aea615f622a5faf6c426486d0db0dfb52
Fauno <pedrohenriqueramossouza@gmail.com>
2024-02-05 03:50:16 -0300

    handle items list dynamically

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
-
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form ondAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +20,7 @@ function Logo() {
   return <h1>🏝 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ ondAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(3);
 
@@ -28,8 +28,8 @@ function Form() {
     event.preventDefault();
     if (!description) return;
 
-    const newItem = { description, quantity, package: false };
-
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    ondAddItems(newItem);
     setDescription("");
   }
 
@@ -54,11 +54,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
